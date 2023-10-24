e7cffd8b67f328bda6fa78bd6b38c546a932f70f
Lucca Mancusi <luccaromano@gmail.com>
2023-10-23 21:46:40 -0300

    register fix

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -2,7 +2,7 @@ import { register } from "../../../services/user";
 
 export default function handler(req, res) {
   try {
-    const newUser = register(req.body);
+    const newUser = register(JSON.parse(req.body));
     res.status(201).json(newUser);
   } catch (err) {
     res.status(400).json(err.message);
